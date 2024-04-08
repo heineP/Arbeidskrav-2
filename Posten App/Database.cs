0d9b4d4cde53d5e468e279c5f75097f88e74a101
heineP <heinep25@gmail.com>
2024-04-09 00:43:26 +0200

    Made temp Database.cs with temp values. Made filepath in Program.cs.

---
+++ Posten App/Database.cs
@@ -0,0 +1,14 @@
+namespace Posten_App;
+
+public class Database
+{
+    // OrdersUnit
+    public string info;
+    // public Package[] packages;
+    
+    
+    // Package values
+    public string description = "asdffdgh";
+    public double[] dimensions = new double[3];
+    public double weight = 123;
+}
--- Posten App/Program.cs
+++ Posten App/Program.cs
@@ -1,5 +1,11 @@
 ﻿
 
-Console.WriteLine("Hello, World!");
-
 Console.WriteLine("BOBO");
+
+string filepath = Directory.GetCurrentDirectory();
+filepath = Directory.GetParent(filepath).FullName;
+filepath = Directory.GetParent(filepath).FullName;
+filepath = Directory.GetParent(filepath).FullName;
+string FILEPATH = Path.Combine(filepath, "items.json");
+
+Console.WriteLine(FILEPATH);
